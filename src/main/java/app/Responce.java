c229c497d2a3ab5a5166cf5e926eb54b0871fcea
JustynaGargula <s221391@student.uek.krakow.pl>
2025-01-30 18:51:08 +0100

    Initialized project

---
+++ src/main/java/app/Responce.java
@@ -0,0 +1,9 @@
+package app;
+
+public interface Responce {
+    String address = "";
+    String bankName = "";
+    String countryISO2 = "";
+    boolean isHeadquarter = false;
+    String swiftCode = "";
+}
